e86c52888af02d0a15b80ed020a6acd4e65336f1
laurent bernabé <laurent.bernabe@gmail.com>
2025-05-15 12:04:34 +0200

    using keyboard events

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
-import { useRef } from 'react';
-import './App.css'
-import Controls from './components/Controls'
-import Scene, { SceneRef } from './components/Scene'
+import { useEffect, useRef } from "react";
+import "./App.css";
+import Controls from "./components/Controls";
+import Scene, { SceneRef } from "./components/Scene";
 
 function App() {
-  const sceneRef = useRef<SceneRef|null>(null);
+  const sceneRef = useRef<SceneRef | null>(null);
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (sceneRef.current) {
+      if (event.key === "ArrowLeft") {
+        sceneRef.current.moveCoinLeft();
+      } else if (event.key === "ArrowRight") {
+        sceneRef.current.moveCoinRight();
+      } else if (event.key === " ") {
+        sceneRef.current.dropNextCoin();
+      }
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown, true);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
-    <div id='root'>
+    <div id="root">
       <Scene ref={sceneRef} />
       <Controls sceneRef={sceneRef} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
